f0223e0a930e68fae7ff3c322fe2e870a2a6c0ed
stanislaw alam <stan.alam@gmail.com>
2024-07-25 15:55:56 -0400

    added comments for react-use-event-hook listener

--- src/hooks/useDontGoAway.ts
+++ src/hooks/useDontGoAway.ts
@@ -1,4 +1,9 @@
 //allow the hook to accept param titles of type array string
+//document title should animate i.e. when the tab loses focus
+// js allows for a visibility change event
+// as soon as the hook mounts; you can begin to listen to events using react's use-event-hook
+//https://www.npmjs.com/package/react-use-event-hook
+// make sure that it fires on mount only -- i.e. 
 export const useDontGoAway = (titles: string[]) => {
   
 
