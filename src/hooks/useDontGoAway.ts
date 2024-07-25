5ab17fb645a6a8d223470b5fccdeb2db67144e3e
stanislaw alam <stan.alam@gmail.com>
2024-07-25 17:14:44 -0400

    added stateful var that changes when visibility changes - need to add function

--- src/hooks/useDontGoAway.ts
+++ src/hooks/useDontGoAway.ts
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react"
 export const useDontGoAway = (titles: string[]) => {
     const [shouldIterateTitles, setShouldIterateTitles] = useState(false) //initial state is false
     const handleVisibilityChange = () => {
-   
+        setShouldIterateTitles(document.visibilityState !== 'visible') //now the stateful var changes when the visibility of the page changes
     }
     useEffect(() => {
         document.addEventListener("visibilitychange", handleVisibilityChange) 
