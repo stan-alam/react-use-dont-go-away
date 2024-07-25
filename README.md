ee3e4985f816197d0127e3ee807a939994ba64ce
stanislaw alam <stan.alam@gmail.com>
2024-07-25 10:13:06 -0400

    added empty hook

---
+++ README.md
@@ -0,0 +1,5 @@
+# React hook - Don't Go Away
+
+```ascii
+a react hook that animates document title when user focus moves  away from page
+```
---
+++ dist/useDontGoAway.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useDontGoAway = void 0;
+var useDontGoAway = function () {
+};
+exports.useDontGoAway = useDontGoAway;
