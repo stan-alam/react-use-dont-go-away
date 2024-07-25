a94e0f49e5765f2ee0dc9b272dc312a60bc0b753
stanislaw alam <stan.alam@gmail.com>
2024-07-25 16:33:43 -0400

    added function handleVisibilityChange to handle visibility changes - hook

--- src/hooks/useDontGoAway.ts
+++ src/hooks/useDontGoAway.ts
@@ -5,10 +5,18 @@
 //https://www.npmjs.com/package/react-use-event-hook
 // make sure that it fires on mount only -- i.e.
 // BEST PRACTICE TO REMOVE REACT HOOK WHEN IT UNMOUNTS -- do this return a function from the use-effect
+//whatever object/state is in this function is fired as part of the unmounting process - use this function's return to remove your event listener
 import {useEffect} from "react"
 export const useDontGoAway = (titles: string[]) => {
+    const handleVisibilityChange = () => {
+        // when the value of the document.visibility state is NOT visible - you should then iteterate through the titles
+        // passed to your hook, Mkay?
+    }
     useEffect(() => {
-        document.addEventListener("visibilitychange", handleVisibilityChange) //the listener function is named handleVisibilityChange
+        document.addEventListener("visibilitychange", handleVisibilityChange) 
+        //the listener function is named handleVisibilityChange
+        return () => {
+            document.removeEventListener("visibilitychange", handleVisibilityChange)
+        }
     }, []) //since this only runs on mount, you will pass an empty array, i.e. empty dependency list to the use-effect-hook
-  
 }
